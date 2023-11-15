59a791438459cbc56e1c4dc6add0226573766577
Pedro Henrique <pedro0123passos@gmail.com>
2023-11-15 12:05:06 -0300

    tuplas

---
+++ tuplas.py
@@ -0,0 +1,15 @@
+#tuplas 
+
+# criando uma tupla 
+
+fruta = ('uva', 'morango', 'banana', 'abacaxi', 'pera'); # cria uma tupla 
+
+print(fruta[0]) # mostra o elemento que está na posição 0
+print('_________________________________________________________________________________________________')
+
+print(fruta.index('abacaxi') )# mostra indece do elemento abacaxi
+print('_________________________________________________________________________________________________')
+
+for i in fruta: # estou percorendo a tupla e mostrando oq tem em cada posição
+    print(i)
+print('_________________________________________________________________________________________________')
